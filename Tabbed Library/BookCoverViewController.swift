3d0e0e7dd5d6605ab6795cc2ac2ca64dbbcee557
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2016-01-28 15:23:05 +0700

    "Fixing the Book Cover View Controller"

--- Tabbed Library/BookCoverViewController.swift
+++ Tabbed Library/BookCoverViewController.swift
@@ -12,7 +12,36 @@ class BookCoverViewController: UIViewController {
     
     // MARK: - Stored Properties
     
-    var book: [String: String]!
+    var book: [String: String]?
+    
+    lazy var bookCoverImage: UIImage? = {
+        var image: UIImage?
+        
+        if self.book == nil {
+            var buffer = [AnyObject]()
+            
+            guard let validPathToResource = NSBundle.mainBundle().pathForResource("Books", ofType: "plist"),  let validAuthors = NSArray(contentsOfFile: validPathToResource) as? [AnyObject] else { return image }
+            for author in validAuthors {
+                guard let validBooks = author["Books"] as? [AnyObject] else { return image }
+                buffer += validBooks
+            }
+            
+            if buffer.count > 0 {
+                let random = Int(arc4random()) % buffer.count
+                
+                if let book = buffer[random] as? [String: String] {
+                    self.book = book
+                }
+            }
+        }
+        
+        if let book = self.book, let fileName = book["Cover"] {
+            image = UIImage(named: fileName)
+        }
+        
+        return image
+    }()
+    
     
     // MARK: - IBOutlet Properties
     
@@ -23,11 +52,11 @@ class BookCoverViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        guard let validBookCover = self.book["Cover"] else { return }
-        self.bookCoverImageView.image = UIImage(named: validBookCover)
+        guard let validBookCoverImage = self.bookCoverImage else { return }
+        self.bookCoverImageView.image = validBookCoverImage
         self.bookCoverImageView.contentMode = UIViewContentMode.ScaleAspectFit
 
-        self.title = self.book["Title"] ?? nil
+        self.title = self.book?["Title"] ?? nil
     }
 
 }
